384b0b572b2b7854125562c713fbcc84b25c15c1
Michael Slice <mjsens523@gmail.com>
2024-05-27 07:45:48 -0500

    Adding User Input Logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+
+
+fn price_option() ->i32 {
+    let mut option_price: f32; 
+    let mut stock_price: f32; 
+    let mut strike_price: f32;
+    let mut risk_free_interest_rate: f32;
+    let mut time_to_maturity: u32;
+    let mut normal_distribution: f32;
+    
+    
+    while {
+        
+    }
+    
+    
+    println!("TEST");
+
+    return 1
+}
+
+
 fn main() {
     println!("Hello, world!");
+
+
+
+
+ price_option();
+
 }
