5671e0cf5e825d223b9c0841f8b917ebf0e66a18
Michael Slice <mjsens523@gmail.com>
2024-05-31 18:38:09 -0500

    Added Binomial Model

--- src/binomialoptions/mod.rs
+++ src/binomialoptions/mod.rs
@@ -25,11 +25,42 @@ impl BinomialOptions {
         volatility
         }
     }
-
-
-
-
-
-
     
+    pub fn binomial_option_model(
+        &self
+        number_of_steps: usize,
+        option_type: char,
+        up_factor: f64,
+        down_factor: f64,
+    ) -> f64 {
+
+        let dt = self.time_to_maturity / number_of_steps as f64;
+        let q = ((self.risk_free_interest_rate * dt).exp() - down_factor) / (up_factor - down_factor);
+        let disc = (-self.risk_free_interest_rate * dt).exp();
+    
+        let mut c: Vec<f64> = Vec::with_capacity(n + 1);
+    
+        for i in 0..=number_of_steps {
+            c.push(self.stock_price * d.powi((n - i) as i32) * u.powi(i as i32));
+        }
+    
+        for i in 0..=number_of_steps {
+            c[i] = c[i].max(0.0);
+        }
+    
+        match option_type {
+            'C' => c.iter_mut().for_each(|val| *val = val.max(0.0)),
+            'P' => c.iter_mut().for_each(|val| *val = val.max(0.0 - self.strike_price)),
+            _ => panic!"Invalid option type, Only C(CALL), or P(PUT) are Valid Options."
+        }
+
+        for i in (1..=number_of_steps).rev() {
+            for j in 0..i { 
+                c[j] = disc * (q * c[j + 1] + (1.0 - q) * c[j]);
+            }
+        }
+    
+        c[0]
+    }
 }
+
--- src/print/mod.rs
+++ src/print/mod.rs
@@ -1,7 +1,6 @@
 use std::io;
 use crate::blackscholes::BlackScholes;
 
-
 pub fn prompt() {
     
     loop {
@@ -13,6 +12,8 @@ pub fn prompt() {
             Ok(number) => {
                 if number == 1 || number == 2 {                
                     if number == 1 {
+                        // Black Scholes Model
+
                         println!("Enter the Stock Price");
                         let mut _stock_price = String::new();
                         io::stdin().read_line(&mut _stock_price).expect("FAILED to read line");
@@ -39,7 +40,7 @@ pub fn prompt() {
                         let _time_to_maturity_num: f64 = _time_to_maturity.trim().parse().expect("Failed to parse input as u32");
                         let _volatility: f64 = _volatility.trim().parse().expect("Failed to parse input as u32");
 
-                        let user = BlackScholes::build_model(
+                        let option_contract = BlackScholes::build_model(
                             _stock_price_num,
                             _strike_price_num,
                             _risk_free_interest_rate_num,
@@ -47,22 +48,52 @@ pub fn prompt() {
                             _volatility
                         );
 
-                        let mut ans: f64 = BlackScholes::price_option_call(&user);
+                        let mut ans: f64 = BlackScholes::price_option_call(&option_contract);
                         println!("The Option Price Call is ${:.2}", ans);
                             
-                        let mut ans: f64 = BlackScholes::price_option_put(&user);
-                        println!("The Option Price Put is ${:.2}", ans);
-                            
+                        let mut ans: f64 = BlackScholes::price_option_put(&option_contract);
+                        println!("The Option Price Put is ${:.2}", ans);       
                     }
                     else {
-                        println!("RUNNING BINOMIAL METHOD");
+                        // Binomial Option Model
                         
-                        
-                        
-                     
-                    }
+                        println!("Enter the Stock Price");
+                        let mut _stock_price = String::new();
+                        io::stdin().read_line(&mut _stock_price).expect("FAILED to read line");
                     
-                    // break;
+                        println!("Enter the Strike Price");
+                        let mut _strike_price = String::new();
+                        io::stdin().read_line(&mut _strike_price).expect("FAILED to read line");
+                    
+                        println!("Enter the Risk-Free Interest Rate (annual)");
+                        let mut _risk_free_interest_rate = String::new();
+                        io::stdin().read_line(&mut _risk_free_interest_rate).expect("FAILED to read line");
+                    
+                        println!("Enter the Time to Expiration (in years)");
+                        let mut _time_to_maturity = String::new();
+                        io::stdin().read_line(&mut _time_to_maturity).expect("FAILED to read line");
+                        
+                        println!("Enter the Stock Volatility (annualized)");
+                        let mut _volatility = String::new();
+                        io::stdin().read_line(&mut _volatility).expect("FAILED to read line");   
+            
+                        let _stock_price_num: f64 = _stock_price.trim().parse().expect("Failed to parse input as u32");
+                        let _strike_price_num: f64 = _strike_price.trim().parse().expect("Failed to parse input as u32");
+                        let _risk_free_interest_rate_num: f64 = _risk_free_interest_rate.trim().parse().expect("Failed to parse input as u32");
+                        let _time_to_maturity_num: f64 = _time_to_maturity.trim().parse().expect("Failed to parse input as u32");
+                        let _volatility: f64 = _volatility.trim().parse().expect("Failed to parse input as u32");                    
+                        
+                        let option_contract = BinomialOptions::build_model(
+                            _stock_price_num,
+                            _strike_price_num,
+                            _risk_free_interest_rate_num,
+                            _time_to_maturity_num,
+                            _volatility
+                        );         
+                        
+                        let mut ans: f64 = BinomialOptions::binomial_option_model(&option_contract);
+                        println!("The Option Price Call is ${:.2}", ans);
+                    }
                 }
                 else {
                     println!("Error you entered {}, please enter either 1, 2, or 3", input);
